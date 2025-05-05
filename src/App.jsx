e2c704ebfe2478a149da7f8efa12ff1c9a10314d
Fumetsu <surajkewat7278@gmail.com>
2025-05-05 19:13:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ const App = () => {
         <div className='app'>
             <Navbar />
 
-            <MovieList type='popular' title='Popular' emoji={Fire} />
-            <MovieList type='top_rated' title='Top Rated' emoji={Star} />
-            <MovieList type='upcoming' title='Upcoming' emoji={Party} />
+            <MovieList type='popular' title='Popular'  />
+            <MovieList type='top_rated' title='Top Rated'  />
+            <MovieList type='upcoming' title='Upcoming' />
         </div>
     );
 };
